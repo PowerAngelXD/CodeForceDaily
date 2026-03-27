c68f047f9019e3467539ca7f426be2dd592933d4
PowerAngelXD <692732163@qq.com>
2026-03-27 14:18:10 +0800

    update: 前一天每日一题题解和代码提交

---
+++ Sieve-of-Erato67henes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t --) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i ++) cin >> a[i];
+
+        if (find(a.begin(), a.end(), 67) != a.end())
+            cout << "YES" << endl;
+        else cout << "NO"<< endl;
+    }
+}
