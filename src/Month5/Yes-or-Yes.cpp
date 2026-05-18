49a6ec1523c0f1b8746342c37f99c2a868186fd1
PowerAngelXD <692732163@qq.com>
2026-05-18 19:46:29 +0800

    update: 今日每日一题代码提交

---
+++ src/Month5/Yes-or-Yes.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t --) {
+        string s;
+        cin >> s;
+
+        int y = 0;
+        for (int i = 0; i < s.length(); i ++) {
+            if (s[i] == 'Y') y ++;
+        }
+
+        if (y <= 1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
